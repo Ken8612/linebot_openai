03b2bcdcf054bc9c84739fef14d53c14ca50fc60
Ken8612 <ilove8888money@gmail.com>
2024-06-25 20:49:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 # Channel Secret
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
+# Dictionary to store user-specific amounts
+user_amounts = {}
+
 # 處理訊息
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -35,12 +38,34 @@ def index():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    try:
-        # 簡單地將使用者的訊息原樣回覆
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
-    except Exception as e:
-        print(traceback.format_exc())
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='發生錯誤，請稍後再試'))
+    user_id = event.source.user_id
+
+    if msg.startswith('記錄金額 '):
+        try:
+            amount = float(msg.split(' ')[1])  # 提取金額
+            if user_id in user_amounts:
+                user_amounts[user_id] += amount
+            else:
+                user_amounts[user_id] = amount
+            reply_msg = f'已記錄金額 {amount}，目前總金額為 {user_amounts[user_id]}'
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg))
+        except ValueError:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='請輸入有效的金額數字'))
+    
+    elif msg == '查詢總金額':
+        if user_id in user_amounts:
+            reply_msg = f'您目前的總金額為 {user_amounts[user_id]}'
+        else:
+            reply_msg = '您尚未有任何記錄的金額'
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg))
+    
+    else:
+        try:
+            # 簡單地將使用者的訊息原樣回覆
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+        except Exception as e:
+            print(traceback.format_exc())
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='發生錯誤，請稍後再試'))
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
